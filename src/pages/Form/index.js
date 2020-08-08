6bd5e9a6869ed777bfde0962938d59d8fe8fc780
Ederson Ferreira <edersondeveloper@gmail.com>
2020-08-07 23:21:27 -0400

    improving leads page desing

--- src/pages/Form/index.js
+++ src/pages/Form/index.js
@@ -1,87 +1,118 @@
-import './styles.css'
-import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import { FiSend } from 'react-icons/fi'
-import api from '../../services/api'
+import './styles.css';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FiSend } from 'react-icons/fi';
+import api from '../../services/api';
 
-import Header from '../../utils/Header'
+import Header from '../../utils/Header';
 
 export default function Form() {
-    const [instName, setInstName] = useState('')
-    const [instId, setInstId] = useState(0)
-    const history = useHistory()
+  const [instName, setInstName] = useState('');
+  const [instId, setInstId] = useState(0);
+  const history = useHistory();
 
-    const [leadName, setLeadName] = useState('')
-    const [leadEmail, setLeadEmail] = useState('')
-    const [leadTel, setLeadTel] = useState('')
+  const [leadName, setLeadName] = useState('');
+  const [leadEmail, setLeadEmail] = useState('');
+  const [leadTel, setLeadTel] = useState('');
 
-    const url_string = window.location.href
-    const url = new URL(url_string)
-    const x = url.searchParams.get("x")
+  const url_string = window.location.href;
+  const url = new URL(url_string);
+  const x = url.searchParams.get('x');
 
-    async function preparePost(){
-        try{
-            const response = await api.get('users')
-            const possibleIds = response.data.users
-            for (let i = 0; i< possibleIds.length; i++){
-                if (possibleIds[i].name === x){
-                    setInstName(possibleIds[i].name)
-                    setInstId(possibleIds[i].loginid)
-                }
-            }
-
-            
-        } catch(err){
-            alert('Falha na conexão, tente novamente mais tarde.')
-        }        
-    }
-
-    async function handleForm(e){
-        e.preventDefault()
-
-        let data = {
-            name: leadName,
-            email: leadEmail,
-            tel: leadTel,
-            contacted: false,
-            interested: false,
-            matriculated: false,
-            view: true,
+  async function preparePost() {
+    try {
+      const response = await api.get('users');
+      const possibleIds = response.data.users;
+      for (let i = 0; i < possibleIds.length; i++) {
+        if (possibleIds[i].name === x) {
+          setInstName(possibleIds[i].name);
+          setInstId(possibleIds[i].loginid);
+        } else {
+          window.location.href = '/';
         }
-        try{
-            await api.post('leads', data, {
-                headers: {
-                    authorization: instId
-                }
-            })
-            history.push('/thanks')
-        }catch(err){
-            alert('Falha na conexão, tente novamente mais tarde')
-        }
+      }
+    } catch (err) {
+      alert('Falha na conexão, tente novamente mais tarde.');
     }
+  }
 
-    window.onload = preparePost
+  async function handleForm(e) {
+    e.preventDefault();
 
-    return(
-        <div className="form-conteiner">
-            <Header />
-            <main className="cutDiv">
-                <div className="formBox" id="formBox">
-                    <span className="blackblock">Preencha o formulário abaixo para ser contactado pela {instName}</span>
-                    <form className="form" onSubmit={handleForm}>
-                        <div className="loginItem"><input className="formInput" type="text" name="name" id="name" placeholder="Seu nome" required
-                        value={leadName}
-                        onChange={e => setLeadName(e.target.value)} /></div>
-                        <div className="loginItem"><input className="formInput" type="email" name="email" id="email" placeholder="Seu E-mail" required
-                        value={leadEmail}
-                        onChange={e => setLeadEmail(e.target.value)} /></div>
-                        <div className="loginItem"><input className="formInput" type="tel" name="tel" id="tel" placeholder="Seu telefone" required
-                        value={leadTel}
-                        onChange={e => setLeadTel(e.target.value)} /></div>
-                        <button type="submit" className="startButton">Enviar <FiSend size={25} color="#fff" /></button>
-                    </form>
-                </div>
-            </main>
+    let data = {
+      name: leadName,
+      email: leadEmail,
+      tel: leadTel,
+      contacted: false,
+      interested: false,
+      matriculated: false,
+      view: true,
+    };
+    try {
+      await api.post('leads', data, {
+        headers: {
+          authorization: instId,
+        },
+      });
+      history.push('/thanks');
+    } catch (err) {
+      alert('Falha na conexão, tente novamente mais tarde');
+    }
+  }
+
+  window.onload = preparePost;
+
+  return (
+    <div className="form-conteiner">
+      <Header />
+      <main className="cutDiv">
+        <div className="formBox" id="formBox">
+          <span className="blackblock">
+            Preencha o formulário abaixo para ser contactado pela {instName}
+          </span>
+          <form className="form" onSubmit={handleForm}>
+            <div className="loginItem">
+              <input
+                className="formInput"
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Seu nome"
+                required
+                value={leadName}
+                onChange={(e) => setLeadName(e.target.value)}
+              />
+            </div>
+            <div className="loginItem">
+              <input
+                className="formInput"
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Seu E-mail"
+                required
+                value={leadEmail}
+                onChange={(e) => setLeadEmail(e.target.value)}
+              />
+            </div>
+            <div className="loginItem">
+              <input
+                className="formInput"
+                type="tel"
+                name="tel"
+                id="tel"
+                placeholder="Seu telefone"
+                required
+                value={leadTel}
+                onChange={(e) => setLeadTel(e.target.value)}
+              />
+            </div>
+            <button type="submit" className="startButton">
+              Enviar <FiSend size={25} color="#fff" />
+            </button>
+          </form>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
--- src/pages/Leads/index.js
+++ src/pages/Leads/index.js
@@ -1,191 +1,265 @@
-import React, { useState, useEffect } from 'react'
-import './styles.css'
-import { FiPower, FiTrash2 } from 'react-icons/fi'
-import { Link, useHistory } from 'react-router-dom'
-import api from '../../services/api'
-import Darkmode from 'darkmode-js'
-const darkmode = new Darkmode()
-darkmode.showWidget()
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
+import Darkmode from 'darkmode-js';
+const darkmode = new Darkmode();
+darkmode.showWidget();
 
-export default function Leads(){
-    const [leads, setLeads] = useState([])
-    const instName = localStorage.getItem('instName')
-    const loginId = localStorage.getItem('loginId')
-    const history = useHistory()
+export default function Leads() {
+  const [leads, setLeads] = useState([]);
+  const instName = localStorage.getItem('instName');
+  const loginId = localStorage.getItem('loginId');
+  const history = useHistory();
 
-    function toggleMenu(e){
-        const navlinks = document.querySelector('div.navlinks')
-        navlinks.classList.toggle('active')
+  function toggleMenu(e) {
+    const navlinks = document.querySelector('div.navlinks');
+    navlinks.classList.toggle('active');
+  }
+
+  useEffect(() => {
+    api
+      .get('leads', {
+        headers: {
+          authorization: loginId,
+        },
+      })
+      .then((response) => {
+        setLeads(response.data);
+      });
+  }, [loginId]);
+
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
+
+  async function handleContacted(leadId, instId) {
+    let data = {
+      leadId: leadId,
+      item: 'contacted',
+    };
+
+    try {
+      api.put('/changeLeadItem', data, {
+        headers: {
+          authorization: instId,
+        },
+      });
+
+      setTimeout(() => {
+        window.location.reload(false);
+      }, 1000);
+    } catch (err) {
+      alert('Falha na conexão, tente novamente mais tarde');
     }
+  }
+  async function handleInterested(leadId, instId) {
+    let data = {
+      leadId: leadId,
+      item: 'interested',
+    };
 
-    useEffect(() => {
-        api.get('leads', {
-            headers: {
-                authorization: loginId
-            }
-        }).then(response => {
-            setLeads(response.data)
-        })
-    }, [loginId] )
+    try {
+      api.put('/changeLeadItem', data, {
+        headers: {
+          authorization: instId,
+        },
+      });
 
-    function handleLogout(){
-        localStorage.clear()
-        history.push('/')
+      setTimeout(() => {
+        window.location.reload(false);
+      }, 1000);
+    } catch (err) {
+      alert('Falha na conexão, tente novamente mais tarde');
     }
+  }
+  async function handleMatriculated(leadId, instId) {
+    let data = {
+      leadId: leadId,
+      item: 'matriculated',
+    };
 
-    async function handleContacted(leadId, instId){
-        let data = ({
-            leadId : leadId,
-            item: 'contacted'
-        })
+    try {
+      api.put('/changeLeadItem', data, {
+        headers: {
+          authorization: instId,
+        },
+      });
 
-        try {
-            api.put('/changeLeadItem', data, {
-                headers: {
-                    authorization: instId
-                }
-            })
-
-            setTimeout(() => {
-                window.location.reload(false)
-            }, 1000)
-        }catch(err){
-            alert('Falha na conexão, tente novamente mais tarde')
-        }
+      setTimeout(() => {
+        window.location.reload(false);
+      }, 1000);
+    } catch (err) {
+      alert('Falha na conexão, tente novamente mais tarde ');
     }
-    async function handleInterested(leadId, instId){
-        let data = ({
-            leadId : leadId,
-            item: 'interested'
-        })
+  }
+  async function handleDelete(leadId, instId) {
+    try {
+      api.delete(`/leads/${leadId}`, {
+        headers: {
+          authorization: instId,
+        },
+      });
 
-        try {
-            api.put('/changeLeadItem', data, {
-                headers: {
-                    authorization: instId
-                }
-            })
-
-            setTimeout(() => {
-                window.location.reload(false)
-            }, 1000)
-        }catch(err){
-            alert('Falha na conexão, tente novamente mais tarde')
-        }
+      setTimeout(() => {
+        window.location.reload(false);
+      }, 1000);
+    } catch (err) {
+      alert('Falha na conexão, tente novamente mais tarde ');
     }
-    async function handleMatriculated(leadId, instId){
-        let data = ({
-            leadId : leadId,
-            item: 'matriculated'
-        })
-
-        try {
-            api.put('/changeLeadItem', data, {
-                headers: {
-                    authorization: instId
-                }
-            })
-
-            setTimeout(() => {
-                window.location.reload(false)
-            }, 1000)
-        }catch(err){
-            alert('Falha na conexão, tente novamente mais tarde ')
-        }
-    }
-    async function handleDelete(leadId, instId){
-        let data = ({
-            leadId : leadId,
-            item: 'view'
-        })
-
-        try {
-            api.put('/changeLeadItem', data, {
-                headers: {
-                    authorization: instId
-                }
-            })
-
-            setTimeout(() => {
-                window.location.reload(false)
-            }, 1000)
-        }catch(err){
-            alert('Falha na conexão, tente novamente mais tarde ')
-        }
-    }
-    
-    return(
-        <div className="leads-conteiner">
-            <header>
-                <nav>
-                <Link to="#" className="toggle-button" id="button" onClick={toggleMenu}>
-                        <span className="bar"></span>
-                        <span className="bar"></span>
-                        <span className="bar"></span>
-                    </Link>
-                    Olá, administrador do(a) {instName}
-                    <div className="navlinks">
-                        <Link to="/"><div className="li">Next</div></Link>
-                    </div>
-                        <FiPower size={30} color='#11548f' className="logout" onClick={handleLogout} />
-                </nav>
-            </header>
-            <main>
-                <span className="blackblock"></span>
-                <div className="leadsBox">
-                    {leads.map(lead => (
-                        lead.view === true ?
-                            lead.contacted === false ?
-                                <div className="lead" key={lead.id}>
-                                    <FiTrash2 size={25} color="#333" className="deleteLead" onClick={ () => handleDelete(lead.id, lead.loginid) } />
-                                    <div className="leadItem">{lead.name}</div><br />
-                                    <div className="leadItem">{lead.email}</div><br />
-                                    <div className="leadItem">{lead.tel}</div>
-                                    <div className="leadItem"><button className="notContacted" onClick={ () => handleContacted(lead.id, lead.loginid) }>Contactar</button></div>
-                                </div>
-                            : lead.contacted === true && lead.interested === true && lead.matriculated === false ?
-                                <div className="lead" key={lead.id}>
-                                    <FiTrash2 size={25} color="#333" className="deleteLead" onClick={ () => handleDelete(lead.id, lead.loginid) } />
-                                    <div className="leadItem">{lead.name}</div><br />
-                                    <div className="leadItem">{lead.email}</div><br />
-                                    <div className="leadItem">{lead.tel}</div>
-                                    <div className="leadItem"><button className="contacted" >Contactado</button></div>
-                                    <div className="leadItem"><button className="interested" >Interessado</button></div>
-                                    <div className="leadItem"><button className="notMatriculated" onClick={ () => handleMatriculated(lead.id, lead.loginid) }>Matriculado</button></div>
-                                </div>
-                            : lead.contacted === true && lead.interested === true && lead.matriculated === true ?
-                                <div className="lead" key={lead.id}>
-                                    <FiTrash2 size={25} color="#333" className="deleteLead" onClick={ () => handleDelete(lead.id, lead.loginid) } />
-                                    <div className="leadItem">{lead.name}</div><br />
-                                    <div className="leadItem">{lead.email}</div><br />
-                                    <div className="leadItem">{lead.tel}</div>
-                                    <div className="leadItem"><button className="contacted" >Contactado</button></div>
-                                    <div className="leadItem"><button className="interested" >Interessado</button></div>
-                                    <div className="leadItem"><button className="matriculated" >Matriculado</button></div>
-                                </div>
-                            : lead.contacted === true && lead.interested === true ?
-                                <div className="lead" key={lead.id}>
-                                    <FiTrash2 size={25} color="#333" className="deleteLead" onClick={ () => handleDelete(lead.id, lead.loginid) } />
-                                    <div className="leadItem">{lead.name}</div><br />
-                                    <div className="leadItem">{lead.email}</div><br />
-                                    <div className="leadItem">{lead.tel}</div>
-                                    <div className="leadItem"><button className="contacted" >Contactado</button></div>
-                                    <div className="leadItem"><button className="interested" >Interessado</button></div>
-                                </div> :
-                                <div className="lead" key={lead.id}>
-                                    <FiTrash2 size={25} color="#333" className="deleteLead" onClick={ () => handleDelete(lead.id, lead.loginid) } />
-                                    <div className="leadItem">{lead.name}</div><br />
-                                    <div className="leadItem">{lead.email}</div><br />
-                                    <div className="leadItem">{lead.tel}</div>
-                                    <div className="leadItem"><button className="contacted" id={lead.id} >Contactado</button></div>
-                                    <div className="leadItem"><button className="notInterested" id={lead.id} onClick={ () => handleInterested(lead.id, lead.loginid) }>Interessado</button></div>
-                                </div> :
-                                null
-
-                    ))}
+  }
+  return (
+    <div className="leads-conteiner">
+      <header>
+        <nav>
+          <Link
+            to="#"
+            className="toggle-button"
+            id="button"
+            onClick={toggleMenu}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </Link>
+          Olá, administrador(a) do(a) {instName}
+          <div className="navlinks">
+            <Link to="/">
+              <div className="li">Next</div>
+            </Link>
+          </div>
+          <FiPower
+            size={30}
+            color="#11548f"
+            className="logout"
+            onClick={handleLogout}
+          />
+        </nav>
+      </header>
+      <main>
+        <span className="blackblock"></span>
+        <h1 className="leadsTitle">Leads</h1>
+        <div className="leadsBox">
+          {leads.map((lead) =>
+            lead.contacted === false ? (
+              <div className="lead" key={lead.id}>
+                <FiTrash2
+                  size={25}
+                  color="#333"
+                  className="deleteLead"
+                  onClick={() => handleDelete(lead.id, lead.loginid)}
+                />
+                <div className="leadItem">{lead.name}</div>
+                <div className="leadItem">{lead.email}</div>
+                <div className="leadItem">{lead.tel}</div>
+                <div className="leadItem">
+                  <button
+                    className="notContacted"
+                    onClick={() => handleContacted(lead.id, lead.loginid)}>
+                    Contactar
+                  </button>
                 </div>
-            </main>
-
+              </div>
+            ) : lead.contacted === true &&
+              lead.interested === true &&
+              lead.matriculated === false ? (
+              <div className="lead" key={lead.id}>
+                <FiTrash2
+                  size={25}
+                  color="#333"
+                  className="deleteLead"
+                  onClick={() => handleDelete(lead.id, lead.loginid)}
+                />
+                <div className="leadItem">{lead.name}</div>
+                <div className="leadItem">{lead.email}</div>
+                <div className="leadItem">{lead.tel}</div>
+                <div className="leadItem">
+                  <button className="contacted">Contactado</button>
+                </div>
+                <div className="leadItem">
+                  <button className="interested">Interessado</button>
+                </div>
+                <div className="leadItem">
+                  <button
+                    className="notMatriculated"
+                    onClick={() => handleMatriculated(lead.id, lead.loginid)}>
+                    Matriculado
+                  </button>
+                </div>
+              </div>
+            ) : lead.contacted === true &&
+              lead.interested === true &&
+              lead.matriculated === true ? (
+              <div className="lead" key={lead.id}>
+                <FiTrash2
+                  size={25}
+                  color="#333"
+                  className="deleteLead"
+                  onClick={() => handleDelete(lead.id, lead.loginid)}
+                />
+                <div className="leadItem">{lead.name}</div>
+                <div className="leadItem">{lead.email}</div>
+                <div className="leadItem">{lead.tel}</div>
+                <div className="leadItem">
+                  <button className="contacted">Contactado</button>
+                </div>
+                <div className="leadItem">
+                  <button className="interested">Interessado</button>
+                </div>
+                <div className="leadItem">
+                  <button className="matriculated">Matriculado</button>
+                </div>
+              </div>
+            ) : lead.contacted === true && lead.interested === true ? (
+              <div className="lead" key={lead.id}>
+                <FiTrash2
+                  size={25}
+                  color="#333"
+                  className="deleteLead"
+                  onClick={() => handleDelete(lead.id, lead.loginid)}
+                />
+                <div className="leadItem">{lead.name}</div>
+                <div className="leadItem">{lead.email}</div>
+                <div className="leadItem">{lead.tel}</div>
+                <div className="leadItem">
+                  <button className="contacted">Contactado</button>
+                </div>
+                <div className="leadItem">
+                  <button className="interested">Interessado</button>
+                </div>
+              </div>
+            ) : lead.contacted == true && lead.interested == false ? (
+              <div className="lead" key={lead.id}>
+                <FiTrash2
+                  size={25}
+                  color="#333"
+                  className="deleteLead"
+                  onClick={() => handleDelete(lead.id, lead.loginid)}
+                />
+                <div className="leadItem">{lead.name}</div>
+                <div className="leadItem">{lead.email}</div>
+                <div className="leadItem">{lead.tel}</div>
+                <div className="leadItem">
+                  <button className="contacted" id={lead.id}>
+                    Contactado
+                  </button>
+                </div>
+                <div className="leadItem">
+                  <button
+                    className="notInterested"
+                    id={lead.id}
+                    onClick={() => handleInterested(lead.id, lead.loginid)}>
+                    Interessado
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <div>Sem leads</div>
+            ),
+          )}
+        </div>
+      </main>
     </div>
-    )
+  );
 }
